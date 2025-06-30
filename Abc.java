e7610c02033196918f2d6a0d26bef4ada94aba1d
shivam <shivam.goyal5010@gmail.com>
2025-06-30 12:07:17 +0530

    dd

---
+++ Abc.java
@@ -0,0 +1,18 @@
+public class Abc {
+    int b;
+    static int s;
+    void m1(){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+
+
+    }
+    public static void main(String[] args)
+    {
+        int a;
+        System.out.println(b);
+    }
+
+    }
+
